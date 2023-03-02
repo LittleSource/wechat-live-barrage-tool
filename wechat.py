5042cbcff3d9c704a81b92a14d696c99a8b6d5ea
LittleSource <444168950@qq.com>
2023-03-02 21:18:09 +0800

    fix

--- wechat.py
+++ wechat.py
@@ -1,41 +1,47 @@
 # -*- coding: utf-8 -*-
-
+import numpy as np
+from ctypes.wintypes import RECT, HWND
+from ctypes import windll, byref, c_ubyte
 import os
 import string
 import time
-import socket
+import websocket
 import requests as requests
-
-############识别文字功能 start############
+import json
+try:
+    import cv2
+except:
+    cv2 = None
+############ 识别文字功能 start############
 from paddleocr import PaddleOCR
+
 
 def getScriptDir():
     return os.path.split(os.path.realpath(__file__))[0]
 
+
 def getText(img_path):
     # Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
     # 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
-    ocr = PaddleOCR(use_angle_cls=True, lang="ch", show_log = False)  # need to run only once to download and load model into memory
+    # need to run only once to download and load model into memory
+    ocr = PaddleOCR(use_angle_cls=True, lang="ch", show_log=False)
     result = ocr.ocr(img_path, cls=True)
     comments = []
     for line in result[0]:
-        textOfThisLine:string = line[1][0]
+        textOfThisLine: string = line[1][0]
         textOfThisLine = textOfThisLine.replace("：", ":")
-        if(":" in textOfThisLine):
+        if (":" in textOfThisLine):
             nameAndComment = tuple(textOfThisLine.split(':', 1))
             comments.append(nameAndComment)
-        
-        if("来了" in textOfThisLine):
+
+        if ("来了" in textOfThisLine):
             nameAndComment = tuple(textOfThisLine.split('来了', 1))
             comments.append(nameAndComment)
     return comments
-############识别文字功能 end############
+############ 识别文字功能 end############
 
-############截图功能 start############
-from ctypes import windll, byref, c_ubyte
-from ctypes.wintypes import RECT, HWND
-import numpy as np
-import cv2
+
+############ 截图功能 start############
 
 GetDC = windll.user32.GetDC
 CreateCompatibleDC = windll.gdi32.CreateCompatibleDC
@@ -49,6 +55,7 @@ DeleteObject = windll.gdi32.DeleteObject
 ReleaseDC = windll.user32.ReleaseDC
 
 windll.user32.SetProcessDPIAware()
+
 
 def capture(handle: HWND):
     r = RECT()
@@ -69,33 +76,39 @@ def capture(handle: HWND):
 
     return np.frombuffer(buffer, dtype=np.uint8).reshape(height, width, 4)
 
-############截图功能 end############
+############ 截图功能 end############
+
 
 def getScriptDir():
     return os.path.split(os.path.realpath(__file__))[0]
 
+
 class Socket():
     def __init__(self):
-        Address = ('127.0.0.1', 25565) # Socket服务器地址,根据自己情况修改
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        Address = "ws://127.0.0.1:5798/ws"  # Socket服务器地址,根据自己情况修改
+        self.ws = websocket.WebSocket()
         try:
-            self.sock.connect(Address)  # 尝试连接服务端
+            self.ws.connect(Address)
         except Exception:
-            print(time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime()) + ' [ERROR] 无法连接到Socket服务器,请检查服务器是否启动')
+            print(time.strftime(
+                "[%Y-%m-%d %H:%M:%S]", time.localtime()) + ' [ERROR] 无法连接到WebSocket服务器,请检查服务器是否启动')
 
     def close(self):
         self.sock.close()
 
     def sendMsg(self, msg):
         try:
-            self.sock.sendall(msg.encode()) # 尝试向服务端发送消息
+            self.ws.send(msg.encode())  # 尝试向服务端发送消息
         except Exception:
-            print(time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime()) + ' [ERROR] 无法连接到Socket服务器,请检查服务器是否启动')
+            print(time.strftime(
+                "[%Y-%m-%d %H:%M:%S]", time.localtime()) + ' [ERROR] 无法连接到WebSocket服务器,请检查服务器是否启动')
+
 
 class Watcher():
     def __init__(self):
         self.monitoringFile = f'{getScriptDir()}/liveFile'
-        self.commentlist = []
+        self.contentlist = []
+        self.mySocket = Socket()
 
     def startWatcher(self):
         index = 1
@@ -103,20 +116,24 @@ class Watcher():
             handle = windll.user32.FindWindowW(None, "视频号直播")
             image = capture(handle)
 
-            sp = image.shape            #获取图像形状：返回【行数值，列数值】列表
-            sz1 = sp[0]                 #图像的高度（行 范围）rows
-            sz2 = sp[1]                 #图像的宽度（列 范围）cols
-            #sz3 = sp[2]                #像素值由【RGB】三原色组成
-            
-            a=int(417) # y start 这个数值按实际来写
-            b=int(sz1) # y end
-            c=int(1171) # x start 这个数值按实际来写
-            d=int(sz2) # x end
-            cropImg = image[a:b,c:d]   #裁剪图像
+            sp = image.shape  # 获取图像形状：返回【行数值，列数值】列表
+            sz1 = sp[0]  # 图像的高度（行 范围）rows
+            sz2 = sp[1]  # 图像的宽度（列 范围）cols
+            # sz3 = sp[2]                #像素值由【RGB】三原色组成
+
+            a = int(600-350)  # y start 这个数值按实际来写417
+            b = int(sz1)  # y end
+            c = int(1040-265)  # x start 这个数值按实际来写1171
+            d = int(sz2)  # x end
+            cropImg = image[a:b, c:d]  # 裁剪图像
             try:
-                cv2.imwrite(self.monitoringFile + '/' +str(index)+".jpg", cropImg)  #
+                cv2.imwrite(self.monitoringFile + '/' +
+                            str(index)+".jpg", cropImg)  #
             except Exception:
-                print("cv2.imwrite func fail! 可能是未打开微信直播工具")
+                print(sz1, sz2)
+                print(cropImg)
+                print("cv2.imwrite func fail! 可能是未打开微信直播工具或权限不足")
+                break
             files = os.listdir(self.monitoringFile)
             if files:
                 for _ in files:
@@ -124,21 +141,29 @@ class Watcher():
                     messages = getText(filepath)
 
                     for message in messages:
-                        userName = message[0]
-                        comment = message[1]
-                        if comment in self.commentlist:
+                        nickname = message[0]
+                        content = message[1]
+                        if len(content) == 0:
                             continue
-                        self.commentlist.append(comment)
-                        msg = f"{userName}:{comment}"
-                        # self.mySocket.sendMsg(msg)
-                        print(msg)
+                        if content in self.contentlist:
+                            continue
+                        self.contentlist.append(content)
+                        if len(self.contentlist) > 20:
+                            del self.contentlist[0:10]
+                        msg = {
+                            "nickname": nickname,
+                            "content": content
+                        }
+                        self.mySocket.sendMsg(
+                            json.dumps(msg, ensure_ascii=False))
+                        print(json.dumps(msg, ensure_ascii=False))
                     try:
                         os.remove(filepath)
                     except PermissionError as e:
                         time.sleep(1)
                         os.remove(filepath)
-            time.sleep(2)
             index += 1
+
 
 if __name__ == '__main__':
     print("web chat running ...")
